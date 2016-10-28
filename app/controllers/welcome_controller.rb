2556526912d069d63fcab39abdeef4b38cdcacb7
Vidhi Ravra <vidhiravra@gmail.com>
2016-10-28 23:37:32 +0530

    Initial Commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,30 @@
+class WelcomeController < ApplicationController
+  
+  def homepage
+  end
+
+  def color01
+  	render 'color01'
+  end
+
+  def color02
+  	render 'color02'
+  end
+
+  def color03
+  	render 'color03'
+  end
+
+  def color04
+  	render 'color04'
+  end
+
+  def color05
+  	render 'color05'
+  end
+
+  def color06
+  	render 'color06'
+  end
+
+end
